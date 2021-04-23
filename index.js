0ffbf3936cadae6b2156bb95555ac8af799b7433
Julien Az <julien.azbrg@gmail.com>
2021-04-23 17:56:37 +0200

    p2

---
+++ index.js
@@ -0,0 +1,33 @@
+let total = 0;
+
+function addition(x) {
+  total += x;
+  return total;
+}
+
+function soustraction(x) {
+  total -= x;
+  return total;
+}
+
+function division(x) {
+  if (total === 0) {
+    return (total = x);
+  } else {
+    total /= x;
+    return total;
+  }
+}
+
+function multiplication(x) {
+  if (total === 0) {
+    return (total = x);
+  } else {
+    total *= x;
+    return total;
+  }
+}
+
+function reset() {
+  total = 0;
+}
