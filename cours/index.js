58de6fd0f1ee6631f5bbbb7396b41d255cc80a32
Julien Az <julien.azbrg@gmail.com>
2021-05-26 12:52:12 +0200

    debut P5

--- cours/index.js
+++ cours/index.js
@@ -1,119 +1,171 @@
-// Définition
+// En JS, tout est un objet, les fonctions, les tableaux, les types nulls, les balises du DOM. On travaille depuis le début avec des objets
 
-// XMLHttpRequest
+// Les bases
+const obj = {
+  // index : value
+  pseudo: "From Scratch",
+  ville: "Bordeaux",
+};
+obj.age = 24;
+// console.log(objet);
+// console.log(objet.ville);
 
-function reqListener() {
-  // console.log(this.responseText);
+//----------------
+// Méthodes objets
+//----------------
+
+// Obtenir les clés
+const keys = Object.keys(obj);
+// console.log(keys);
+
+// Obtenir les valeurs
+const values = Object.values(obj);
+// console.log(values);
+
+// Créer un tableau de chaque objet+valeur
+const nestedArray = Object.entries(obj);
+// console.log(nestedArray);
+
+const obj2 = {
+  taille: "1m80",
+  poids: "76kg",
+};
+
+// Fusionner tableau
+const fusion = Object.assign({}, obj, obj2);
+// console.log(fusion);
+
+// Empeche les modifications
+const newObj = Object.freeze(obj);
+newObj.pseudo = "Test";
+newObj.adresse = "24 avenue du Code";
+// console.log(obj);
+
+// 3 FACONS DE CONSTRUIRE UN OBJET
+// Fonction constructeur
+function User(pseudo, ville) {
+  this.pseudo = pseudo;
+  this.ville = ville;
+}
+// This keyword, permet de stocker une valeur
+// Créer nouvelle instance avec New
+const user1 = new User("From Scratch", "Bordeaux");
+
+// console.log(user1.pseudo);
+
+//----------------
+// PARENTHESE THIS
+//----------------
+// This fait référence à l'objet, il change selon qui l'appelle. Fait référence à l'instance de la classe
+
+//-------------------------
+// Factory functions
+function User3(pseudo, ville) {
+  return {
+    pseudo,
+    ville,
+  };
 }
 
-let req = new XMLHttpRequest();
-req.onload = reqListener;
-// req.open("get", "data.json", true);
-req.open("get", "data.txt", true);
-req.send();
+// const user3 = User3("From", "Nice");
+// console.log(user3);
 
-// Fetch
+// Class syntax (depuis ES6)
+// https://everyday.codes/javascript/please-stop-using-classes-in-javascript/
+class User2 {
+  constructor(pseudo, ville) {
+    this.pseudo = pseudo;
+    this.ville = ville;
+  }
+}
+const user2 = new User2("FS", "Lyon");
+// console.log(user2);
 
-// fetch("url", "options").then(
-//   (response) => {
-//     // response
-//   }).catch((err) => console.log(err))
-// );
+//-------------
+// Les méthodes
+//-------------
 
-// fetch("data.txt")
-//   .then((res) => res.text())
-//   .then((data) => console.log(data));
+function User4(pseudo, ville) {
+  this.pseudo = pseudo;
+  this.ville = ville;
 
-// fetch("data.json")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
-
-//---------------
-// Projet Blagues
-//---------------
-
-// L'interface Headers vous permet de créer vos propres objets d'en-têtes via le constructeur Headers
-
-const myHeaders = new Headers();
-
-const init = {
-  method: "GET", // POST, PUT, DELETE
-  headers: myHeaders,
-  mode: "cors",
-  cache: "default",
+  this.sayMyName = function () {
+    console.log("Bonjour je suis " + pseudo);
+  };
+}
+User4.prototype.sayGoodbye = function () {
+  console.log("Adios");
 };
-fetch("data.json", init).then((res) => res.json());
-// .then((data) => console.log(data));
-
-//--------------------------
-// npm i -g json-server
-// json-server --w db.json
-
-let init2 = {
-  method: "POST", // POST, PUT, DELETE
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    pseudo: "From Scratch",
-    message: "Yo les gens !",
-  }),
-  mode: "cors",
-  credentials: "same-origin",
-};
-
-document.querySelector("form").addEventListener("submit", (e) => {
-  // e.preventDefault();
-  fetch("http://localhost:3000/posts", init2);
+// sayGoodbye est dans le prototype
+Object.assign(User4.prototype, {
+  method1() {},
+  method2() {},
+  method3() {},
 });
+const person = new User4("Patrice", "Nantes");
+// console.log(person);
+// person.sayMyName();
 
-//-----------
-// Asycnhrone
-//-----------
+//---------------------
+// Parenthèse Prototype
 
-setTimeout(() => {
-  // console.log("Test !");
-}, 2000);
+const array = ["one", "two", "three"];
+// console.log(array);
+const array2 = new Array("one", "two", "three");
+//----------------------------------------------
 
-// Promise
-// fetch('monlien').then(() => ...)
+class User5 {
+  constructor(pseudo, ville) {
+    this.pseudo = pseudo;
+    this.ville = ville;
 
-// async/await
-async function fetchData() {
-  // await fetch('monlien')
-  // await maFonction();
-  // Execute ensuite...
+    this.sayMyName = function () {
+      console.log("Bonjour je suis " + pseudo);
+    };
+  }
+
+  // Ecrire la méthode dans le prototype
+  sayMyName2() {
+    console.log("Bonjour je suis " + this.pseudo);
+  }
 }
-const fetchData2 = async () => {
-  // await fetch('monlien')
-  // await maFonction();
-  // Execute ensuite...
-};
 
-//---------
-// User Api
-//---------
+const utilisateur = new User5("Denis", "Lille");
+// console.log(utilisateur);
+// utilisateur.sayMyName();
 
-//--------------------------------
-// JSON
-// JSON = format de données
+// sayMyName2 est dans le prototype (voir consolelog)
+// utilisateur.sayMyName2();
 
-// méthode .json() Elle retourne une promesse qui s'auto-résout en renvoyant le corps de la requête parsée au format JSON.
+// Methode dans l'objet VS dans le proto
+// Dans le proto 30% plus rapide que dans l'objets (peu important avec techno moderne)
 
-fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    let settings = JSON.stringify(data);
-    console.log(JSON.parse(settings));
-  });
+//-----------
+// L'héritage
+//-----------
 
-//---------
-// Meal Api
-//---------
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  saySomething(text) {
+    console.log(this.name + " dit : " + text);
+  }
+}
 
-//---------------------------------
-// Web API
+class Dog extends Animal {
+  // constructor(name, age) {
+  //   super(name, age)
+  // }
+  // Super appelle la classe parente
 
-// Local storage
+  run() {
+    console.log(this.name + " court");
+  }
+}
 
-// Cookie + notif
+const rex = new Dog("Rex", 9);
+// console.log(rex);
+// rex.saySomething("Wouaf !");
+// rex.run();
